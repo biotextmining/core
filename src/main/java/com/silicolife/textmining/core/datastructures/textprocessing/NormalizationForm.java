be4e02b75364955b566696474a9b21c2f50ff576
Ruben Rodrigues <rrodrigues@silicolife.com>
2016-04-19 15:22:49 +0100

    [BugFix] Text offsets problem resolver bug in < and > replaced by strange chars.

--- src/main/java/com/silicolife/textmining/core/datastructures/textprocessing/NormalizationForm.java
+++ src/main/java/com/silicolife/textmining/core/datastructures/textprocessing/NormalizationForm.java
@@ -37,31 +37,31 @@ public class NormalizationForm {
 //	}
 	
 	public static String removeOffsetProblemSituation(String text) {
-		text = text.replaceAll("\\r", "").trim();
-		text = text.replaceAll("\\n\\n", ". ");
-		text = text.replaceAll("\\n", " "); 
-		text = text.replaceAll("\\s", " ");
-		text = text.replaceAll("\\t", " ");
-		text = text.replaceAll("\\f", "");			
-		text = text.replaceAll("\\e", "");
-		text = text.replaceAll("\\c[", "");
-		text = text.replaceAll("<", "«");
-		text = text.replaceAll(">", "»");
-		text = text.replaceAll("·", ".");
-		text = text.replaceAll("\\Q", "");	
-		text = text.replaceAll("\\.{2,}\\s", ". ");
-		text = text.replaceAll("-\\r{0,1}\\n", "");
-		text = text.replaceAll("\\.([a-zA-Z]{1,})", ". $1");
-		text = text.replaceAll("’", "");
-		text = text.replaceAll("\u2019", "'");
-		text = text.replaceAll("\\s+", " ");
-		text = text.replaceAll("([a-zA-Z]{3,})-\\s{1}([a-zA-Z]{3,})", "$1$2"); 
-		text = text.replaceAll("("+numbers+")\\s{0,1}[×x]\\s{0,1}10\\s{0,1}(\\d+)", "$1 x 10exp$2"); 
-		text = text.replaceAll( "([\\ud800-\\udbff])", "");
-		text = text.replaceAll( "([\\udc00-\\udfff])", "");
-		text = text.replaceAll( "−", "-"); // Added to Ana project
-		return text;
-	}
+	text = text.replaceAll("\\r", "").trim();
+	text = text.replaceAll("\\n\\n", ". ");
+	text = text.replaceAll("\\n", " "); 
+	text = text.replaceAll("\\s", " ");
+	text = text.replaceAll("\\t", " ");
+	text = text.replaceAll("\\f", "");			
+	text = text.replaceAll("\\e", "");
+	text = text.replaceAll("\\c[", "");
+	text = text.replaceAll("«", "<");
+	text = text.replaceAll("»", ">");
+	text = text.replaceAll("·", ".");
+	text = text.replaceAll("\\Q", "");	
+	text = text.replaceAll("\\.{2,}\\s", ". ");
+	text = text.replaceAll("-\\r{0,1}\\n", "");
+	text = text.replaceAll("\\.([a-zA-Z]{1,})", ". $1");
+	text = text.replaceAll("’", "");
+	text = text.replaceAll("\u2019", "'");
+	text = text.replaceAll("\\s+", " ");
+	text = text.replaceAll("([a-zA-Z]{3,})-\\s{1}([a-zA-Z]{3,})", "$1$2"); 
+	text = text.replaceAll("("+numbers+")\\s{0,1}[×x]\\s{0,1}10\\s{0,1}(\\d+)", "$1 x 10exp$2"); 
+	text = text.replaceAll( "([\\ud800-\\udbff])", "");
+	text = text.replaceAll( "([\\udc00-\\udfff])", "");
+	text = text.replaceAll( "−", "-"); // Added to Ana project
+	return text;
+}
 	
 	
 	
