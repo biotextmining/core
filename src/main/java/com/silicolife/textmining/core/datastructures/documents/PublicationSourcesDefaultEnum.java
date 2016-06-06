1da5d88090e3026ee0fc3b0442cfcb2a9f8a0b6e
Hugo Costa <hcosta@silicolife.com>
2016-06-06 12:21:50 +0100

    [add] Publication Source Default Enum

---
+++ src/main/java/com/silicolife/textmining/core/datastructures/documents/PublicationSourcesDefaultEnum.java
@@ -0,0 +1,15 @@
+package com.silicolife.textmining.core.datastructures.documents;
+
+/**
+ * Enum class to save the identifiers for some publications external Ids
+ * 
+ * @author Hugo Costa
+ *
+ */
+public enum PublicationSourcesDefaultEnum {
+	DOI,
+	PUBMED,
+	pmc,
+	patent,
+	epodoc;
+}
