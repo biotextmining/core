e4feabe9b9654c5a3733687ad273746f447c5af1
Ruben Rodrigues <rrodrigues@silicolife.com>
2016-08-10 16:05:32 +0100

    [Update] ann_abbreviation field in sql update

--- src/test/resources/dbupdatefolder/anote2_db_update_4.sql
+++ src/test/resources/dbupdatefolder/anote2_db_update_4.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `annotations` 
 DROP COLUMN `ann_normalization_form`,
-ADD COLUMN `ann_abreviation` TINYINT(1) NULL DEFAULT 0 AFTER `ann_notes`;
+ADD COLUMN `ann_abbreviation` TINYINT(1) NULL DEFAULT 0 AFTER `ann_notes`;
