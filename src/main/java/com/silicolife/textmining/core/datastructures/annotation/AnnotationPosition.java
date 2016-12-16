e1fcc8a578c3f6b98a13e372536077ca9b59d3ba
Hugo Costa <hcosta@silicolife.com>
2016-12-16 19:10:52 +0000

    [update] Annotation Position class with hash code method

--- src/main/java/com/silicolife/textmining/core/datastructures/annotation/AnnotationPosition.java
+++ src/main/java/com/silicolife/textmining/core/datastructures/annotation/AnnotationPosition.java
@@ -9,6 +9,22 @@ package com.silicolife.textmining.core.datastructures.annotation;
  */
 public class AnnotationPosition implements Cloneable,Comparable<AnnotationPosition>{
 	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AnnotationPosition other = (AnnotationPosition) obj;
+		if (end != other.end)
+			return false;
+		if (start != other.start)
+			return false;
+		return true;
+	}
+
 	private int start;
 	private int end;
 	private String originalText;
@@ -98,5 +114,14 @@ public class AnnotationPosition implements Cloneable,Comparable<AnnotationPositi
 	public int diference()
 	{
 		return getEnd()-getStart();
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + end;
+		result = prime * result + start;
+		return result;
 	}
 }
