0437e69e07b3cf2f145b31ce568244d8e88be8b7
Hugo Costa <hcosta@silicolife.com>
2017-01-04 16:35:20 +0000

    [add] IEprocess hash code method

--- src/main/java/com/silicolife/textmining/core/datastructures/process/IEProcessImpl.java
+++ src/main/java/com/silicolife/textmining/core/datastructures/process/IEProcessImpl.java
@@ -173,4 +173,26 @@ public class IEProcessImpl extends Observable implements IIEProcess {
 		}
 		return prop;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		IEProcessImpl other = (IEProcessImpl) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 }
