34a9cb2ad35de2187e54f669a277489ea41c51c0
Hugo Costa <hcosta@silicolife.com>
2017-01-16 17:29:37 +0000

    [update] SentenceImpl with hashcode and equals methods

--- src/main/java/com/silicolife/textmining/core/datastructures/documents/structure/SentenceImpl.java
+++ src/main/java/com/silicolife/textmining/core/datastructures/documents/structure/SentenceImpl.java
@@ -129,4 +129,29 @@ public class SentenceImpl implements ISentence{
 		}
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (endOffset ^ (endOffset >>> 32));
+		result = prime * result + (int) (startOffset ^ (startOffset >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SentenceImpl other = (SentenceImpl) obj;
+		if (endOffset != other.endOffset)
+			return false;
+		if (startOffset != other.startOffset)
+			return false;
+		return true;
+	}
+
 }
