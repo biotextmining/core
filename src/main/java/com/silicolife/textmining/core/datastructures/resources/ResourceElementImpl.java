7fefb9a28d76efa1dc5096168f46a23c23761412
Hugo Costa <hcosta@silicolife.com>
2017-01-18 11:08:44 +0000

    [update] Resource Element class with hashcode and equals

--- src/main/java/com/silicolife/textmining/core/datastructures/resources/ResourceElementImpl.java
+++ src/main/java/com/silicolife/textmining/core/datastructures/resources/ResourceElementImpl.java
@@ -207,4 +207,26 @@ public class ResourceElementImpl implements IResourceElement{
 	public void generateNewId() {
 		this.id = GenerateRandomId.generateID();		
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ResourceElementImpl other = (ResourceElementImpl) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 }
