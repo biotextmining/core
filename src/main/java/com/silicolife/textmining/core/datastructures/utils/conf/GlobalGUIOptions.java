ec194bf451b0442ae042282daf5648f708dac4f3
Hugo Costa <hcosta@silicolife.com>
2017-02-10 16:13:14 +0000

    [refatoring] Global option ( Some moved to Anote project) [refactoring] Global GUI ( moved to @note)

--- src/main/java/com/silicolife/textmining/core/datastructures/utils/conf/GlobalGUIOptions.java
+++
@@ -1,7 +0,0 @@
-package com.silicolife.textmining.core.datastructures.utils.conf;
-
-public class GlobalGUIOptions {
-	
-	public static final int tableSelectionSize = 25;
-	public static final int popupMassagesTime = 2000;
-}
