34c4fce2e6ed6dd80acaa0b2d165a07905de109f
Hugo Costa <hcosta@silicolife.com>
2017-03-09 11:03:40 +0000

    [bug fix] remove files from views that was removed in db

--- src/test/resources/anote2databasescript.sql
+++ src/test/resources/anote2databasescript.sql
@@ -1005,8 +1005,6 @@ CREATE TABLE IF NOT EXISTS `corpus_has_publications_has_processes` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
-
 -- -----------------------------------------------------
 -- Placeholder table for view `resources_class_stats`
 -- -----------------------------------------------------
@@ -1126,7 +1124,7 @@ GROUP BY ann_process_id,ann_element,ann_class_id
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `process_clues_stats`;
 
-CREATE  OR REPLACE VIEW process_clues_stats AS SELECT ann_process_id,ann_clue,COUNT(ann_clue) as numberClues ,ann_clue FROM annotations 
+CREATE  OR REPLACE VIEW process_clues_stats AS SELECT ann_process_id,ann_clue,COUNT(ann_clue) as numberClues FROM annotations 
 WHERE ann_active=1 AND ann_annot_type='re'
 GROUP BY ann_process_id,ann_clue
 
