103765c9ee0bd32b2f854a03f9f85c9c29751f25
RRodrigues <Ru13en@users.noreply.github.com>
2017-05-04 12:34:06 +0100

    [Add] is finished boolean to IParallelJob

--- src/main/java/com/silicolife/textmining/core/datastructures/utils/multithearding/IParallelJob.java
+++ src/main/java/com/silicolife/textmining/core/datastructures/utils/multithearding/IParallelJob.java
@@ -3,6 +3,7 @@ package com.silicolife.textmining.core.datastructures.utils.multithearding;
 public interface IParallelJob<T> extends Runnable{
 	
 	public T getResultJob();
+	public boolean isFinished();
 	public void kill();
 
 }
