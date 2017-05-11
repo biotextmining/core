187b49323f48304d15d8430eaadb28cc7134d0d7
RRodrigues <Ru13en@users.noreply.github.com>
2017-05-11 15:36:31 +0100

    [update] link was incomplete

--- src/test/resources/dbupdatefolder/anote2_db_update_7.sql
+++ src/test/resources/dbupdatefolder/anote2_db_update_7.sql
@@ -1,3 +1,3 @@
 UPDATE `hyper_link_menus` SET `hyl_hyper_link_menu` = 'http://www.ensembl.org/Multi/Search/Results?q=#' WHERE `hyl_id` = 2;
-UPDATE `hyper_link_menus` SET `hyl_hyper_link_menu` = 'http://amigo.geneontology.org/amigo/medial_search?#' WHERE `hyl_id` = 8;
+UPDATE `hyper_link_menus` SET `hyl_hyper_link_menu` = 'http://amigo.geneontology.org/amigo/medial_search?q=#' WHERE `hyl_id` = 8;
 UPDATE `hyper_link_menus` SET `hyl_hyper_link_menu` = 'http://pfam.xfam.org/search/keyword?query=#&submit=Submit' WHERE `hyl_id` = 13;
