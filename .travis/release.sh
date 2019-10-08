c5232b29f056a4d2f6f0a024f797d0831eb3b2bf
Paulo Vilaça <paulo.r.c.vilaca@gmail.com>
2019-10-08 17:34:58 +0100

    [CI] update

---
+++ .travis/release.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Change pom version to $TRAVIS_TAG"
+mvn --batch-mode versions:set -DnewVersion=${TRAVIS_TAG}
+
+echo "Uploading to oss repo and GitHub"
+mvn -s .travis/settings.xml --batch-mode clean deploy -P release -DskipTests
