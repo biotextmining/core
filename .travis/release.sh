0bac69ebed323a061511c1d2a498abb36d06d62f
Paulo Vilaça <paulo.r.c.vilaca@gmail.com>
2019-10-14 14:20:14 +0100

    [CI] changed settings.xml and release.sh to travis-ci deployment

--- .travis/release.sh
+++ .travis/release.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-echo "Change pom version to $TRAVIS_TAG"
+echo "[DEPLOY] =========== Change pom version to $TRAVIS_TAG"
 mvn --batch-mode versions:set -DnewVersion=${TRAVIS_TAG}
 
-echo "Uploading to oss repo and GitHub"
-mvn -s .travis/settings.xml --batch-mode clean deploy -P release -DskipTests
+echo "[DEPLOY] =========== Uploading to UM repo"
+mvn -s .travis/settings.xml --batch-mode -P um-deploy clean deploy -DskipTests || true
+
+echo "[DEPLOY] =========== Uploading to oss.sonatype.org"
+mvn -s .travis/settings.xml --batch-mode -P central-deploy clean deploy -DskipTests
+
+
+
