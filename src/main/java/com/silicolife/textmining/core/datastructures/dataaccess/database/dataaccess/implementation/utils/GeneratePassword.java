ab01de9ab6ec77cf9be607bc34c6006af2f9e15c
Ruben Rodrigues <ru13en_12@hotmail.com>
2020-09-14 14:25:20 +0100

    [Add] main to GeneratePassword to help updating the security hashs

--- src/main/java/com/silicolife/textmining/core/datastructures/dataaccess/database/dataaccess/implementation/utils/GeneratePassword.java
+++ src/main/java/com/silicolife/textmining/core/datastructures/dataaccess/database/dataaccess/implementation/utils/GeneratePassword.java
@@ -31,4 +31,19 @@ public class GeneratePassword {
 	//		System.out.print(GeneratePassword.generate("teste", salt));
 	//
 	//	}
+	
+	public static void main(String[] args) throws Exception {
+		if(args.length != 1) {
+			String message = "The GeneratePassword command must have <passwordToHash>, given:";
+	        for (String s: args) 
+	        	message = message + " " + s;
+	        
+	        throw new Exception(message);
+		}
+		
+		String password = args[0];
+		String genpass = GeneratePassword.generate(password);
+		System.out.println("Please replace the generated hash to auth_users table in the line of the user to be modified:");
+		System.out.println(genpass);
+	}
 }
